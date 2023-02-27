cdc693c8538784e72a921654729ab7a946eff5a7
luisaferco <luisa.colorado.e@gmail.com>
2023-02-26 23:16:20 -0500

    test cases added

--- src/test/java/com/automation/training/tests/NavigationTest.java
+++ src/test/java/com/automation/training/tests/NavigationTest.java
@@ -1,19 +1,31 @@
 package com.automation.training.tests;
 
-import com.automation.training.BaseMobileTest;
-import org.springframework.context.annotation.Description;
+import com.automation.training.pages.SearchPage;
+import com.automation.training.pages.WikiHomePage;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class NavigationTest extends BaseMobileTest {
 
-    /**
-     * Validate Map Screen Navigation.
-     */
-    @Description(value = "Map screen Navigation test")
-    @Test()
-    public void navigateToMapScreen() {
-        log.info("Start Navigation to Map Screen");
 
+    @Test(description = "Search navigation test")
+    @Parameters({"searchCriteria"})
+    public void navigateToSearchPage(String searchCriteria) {
+        WikiHomePage wikiHomePage = getWikiHomePage();
+        SearchPage searchPage = wikiHomePage.openSearch().search(searchCriteria);
+        Assert.assertTrue(searchPage.getResultsSize() > 0,
+                "Expected that the search results contains some coincidences");
     }
+
+    @Test(description = "Open search details from the result list")
+    @Parameters({"searchCriteria"})
+    public void navigateToSearchDetails(String searchCriteria) {
+        WikiHomePage wikiHomePage = getWikiHomePage();
+        SearchPage searchPage = wikiHomePage.openSearch().search(searchCriteria);
+        Assert.assertTrue(searchPage.getResultByIndex(0).getResultsTitleSelected().contains(searchCriteria),
+                "Expected that search result selected contains search criteria");
+    }
+
 
 }
