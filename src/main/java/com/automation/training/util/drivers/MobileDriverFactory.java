eba7584e7eba9ad6bc4b0ae0676cb2fb5af42ae2
luisa.colorado <luisa.colorado.e@gmail.com>
2023-03-11 15:16:38 -0500

    create driver from parameter test

--- src/main/java/com/automation/training/util/drivers/MobileDriverFactory.java
+++ src/main/java/com/automation/training/util/drivers/MobileDriverFactory.java
@@ -1,9 +1,7 @@
 package com.automation.training.util.drivers;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
-import io.appium.java_client.remote.MobilePlatform;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 /**
  * Get driver whether environment variable has been configured.
@@ -12,44 +10,15 @@ import io.appium.java_client.remote.MobilePlatform;
  */
 public class MobileDriverFactory extends ConfigCapabilities {
 
-    private MobileDriverFactory(String platformName){
-        super(platformName);
+    public MobileDriverFactory(){
+        super();
     }
 
-    public static MobileDriverFactory getInstanceFactory(){
-        return new MobileDriverFactory(MobilePlatform.ANDROID);
-    }
-
-    public AppiumDriver<?> getDriver(){
-        AppiumServerAddress serverAddress = new AppiumServerAddress();
-        serverAddress.setAddress();
-        return new AndroidDriver<AndroidElement>(serverAddress.getRemoteAddress(), super.uploadCapabilities());
+    public AppiumDriver<?> getDriver(AppiumConfig config){
+        AppiumServerAddress address = new AppiumServerAddress(config.host(), config.port());
+        address.setAddress();
+        DesiredCapabilities capabilities = super.uploadCapabilities(config.platformName());
+        return MobileType.valueOf(config.platformName().toUpperCase()).setDriver(address, capabilities);
     }
 }
 
-/*enum MobileDriver {
-
-    ANDROID("Android",(address) -> {
-            return new AndroidDriver<AndroidElement>(address.getRemoteAddress());
-    }),
-    IOS ("iOS",(AppiumServerAddress address, DesiredCapabilities desiredCapabilities) ->
-            new IOSDriver<IOSElement>(address.getRemoteAddress(),desiredCapabilities);
-    );
-
-enum MobileDriver {
-
-    ANDROID {
-        @Override
-        public AndroidDriver<AndroidElement> setDriver(){
-            return SetAndroidDriver.getInstance().createDriver();
-        }
-    },
-    IOS {
-        @Override
-        public IOSDriver<IOSElement> setDriver(){
-            return null;
-        }
-    };
-
-
- */
