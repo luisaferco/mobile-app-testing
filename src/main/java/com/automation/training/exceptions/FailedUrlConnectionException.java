017017a41932a5b1792cfaf8b6a051ab815aa9a4
luisa.colorado <luisa.colorado.e@gmail.com>
2023-03-16 16:21:48 -0500

    comments added

--- src/main/java/com/automation/training/exceptions/FailedUrlConnectionException.java
+++ src/main/java/com/automation/training/exceptions/FailedUrlConnectionException.java
@@ -1,5 +1,8 @@
 package com.automation.training.exceptions;
 
+/**
+ * Custom exception
+ */
 public class FailedUrlConnectionException extends AssertionError {
 
     public static final String FAILED_URL_CALLING = "Failed url calling: %s";
