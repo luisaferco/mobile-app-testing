481b4fc0dffd072f032ed60199e153bc9374dc46
luisa.colorado <luisa.colorado.e@gmail.com>
2023-03-16 16:19:48 -0500

    explicit wait added, element type updated due to the new appium version

--- src/main/java/com/automation/training/pageobject/BasePage.java
+++ src/main/java/com/automation/training/pageobject/BasePage.java
@@ -1,22 +1,32 @@
 package com.automation.training.pageobject;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
+
 import static org.openqa.selenium.support.PageFactory.initElements;
 
-
+/**
+ * The BasePage abstract class contains overrides constructor and
+ * provides the Appium Driver
+ * @param <T> : MobileDriver
+ * @author luisaferco
+ */
 public abstract class BasePage<T extends AppiumDriver> {
 
     private static final int TIMEOUT = 10;
     protected final T driver;
+    protected WebDriverWait wait;
 
 
-    public BasePage(AppiumDriver<?> driver) {
+    public BasePage(AppiumDriver driver) {
         this.driver = (T) driver;
+        this.wait = new WebDriverWait(driver,Duration.ofSeconds(TIMEOUT));
         initElements(new AppiumFieldDecorator(
                 driver, Duration.ofSeconds(0)), this);
     }
@@ -31,25 +41,28 @@ public abstract class BasePage<T extends AppiumDriver> {
         }
     }
 
-    public void refresh() {
-        driver.launchApp();
+    public void terminateApp(String packageName) {
+        AndroidDriver driver1 = (AndroidDriver) getDriver();
+        driver1.terminateApp(packageName);
+
     }
 
     /**
      *  custom click
-     * @param element : MobileElement
-     * @author luisaferco
+     * @param element : WebElement
      */
-    public void click(MobileElement element) {
-        WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
+    public void click(WebElement element) {
         wait.until(ExpectedConditions.visibilityOf(element));
         element.click();
     }
 
-    public void type(MobileElement element, String text) {
-        WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
+    /**
+     *  custom type
+     * @param element : WebElement
+     */
+    public void type(WebElement element, String text) {
         wait.until(ExpectedConditions.visibilityOf(element));
-        element.setValue(text);
+        element.sendKeys(text);
     }
 
 }
